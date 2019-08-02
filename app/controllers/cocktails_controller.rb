4c80fd02e45cf31060ce65cb204e9f6052e1bb2a
Dwesmond Ho <desho8@gmail.com>
2019-08-02 15:47:24 +0800

    Changes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,41 @@
+class CocktailsController < ApplicationController
+  # before_action :find_ingredient, only: [ :dose ]
+
+  # def dose
+  #   @dose_name = @ingredient.dose
+  # end
+
+  # private
+
+  # def find_cocktail
+  #   @cocktail = Cocktail.find(params[:id])
+  # end
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
